f301183e07b748184c0acc930acb82254e725569
AMAN KHAN <124271834+TAIHAN86@users.noreply.github.com>
2024-01-04 23:59:22 +0530

    react router dom library install

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react-dom/client';
+import ReactDOM from 'react-router-dom';
 import App from './App';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <browserRouter> 
+   <App />
+  </browserRouter>
+   
+  
 )
